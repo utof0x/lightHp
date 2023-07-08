d93145b5f3ba52613dbba2cc21263b47e617f881
utof0x <popovychmykhailo00@gmail.com>
2023-07-08 21:53:35 +0300

    created routing system

---
+++ core/Request.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\core;
+
+class Request
+{
+    public function getPath(): string
+    {
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $position = strpos($path, '?');
+
+        if (!$position) {
+            return $path;
+        }
+
+        return substr($path, 0, $position);
+    }
+
+    public function getMethod(): string
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+}
